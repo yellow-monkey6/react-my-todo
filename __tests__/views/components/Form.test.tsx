fcf46927ee8a52b311603cea251a86785c5601af
大谷周平 <yellow.monkey.type.f@gmail.com>
2023-11-03 18:37:05 +0900

    UIコンポーネントテストのイベント発火の例を追加
    テストコードのカバレッジが66%でイエローとなっていた原因として表示系やイベント系のテストケースが不足していたため、これらを追加

--- __tests__/views/components/Form.test.tsx
+++ __tests__/views/components/Form.test.tsx
@@ -1,8 +1,20 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { Form } from "../../../src/views/components/Form";
 
 test("名前の表示", () => {
     render(<Form name="taro" />);
     // 特定のDOMの要素を取得するアサーション
     expect(screen.getByText("taro")).toBeInTheDocument();
+});
+
+test("見出しの表示", () => {
+    render(<Form name="taro" />);
+    expect(screen.getByRole("heading")).toHaveTextContent("アカウント情報");
+});
+
+test("ボタンをクリックすると、イベントハンドラーが実行される", () => {
+    const mockFn = jest.fn();
+    render(<Form name="taro" onSubmit={mockFn} />);
+    fireEvent.click(screen.getByRole("button"));    
+    expect(mockFn).toHaveBeenCalled();
 });
